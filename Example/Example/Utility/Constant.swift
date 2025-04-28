aa61f11192187359679b807ef7d581150ca49c65
William.Weng <William.Weng@iit.com.hk>
2025-04-28 16:00:33 +0800

    - 加上記憶功能 (lastContext)

---
+++ Example/Example/Utility/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  Example
+//
+//  Created by iOS on 2025/4/28.
+//
+
+import Foundation
+
+extension ViewController {
+    
+    enum TextFieldType: Int {
+        case ip = 0
+        case port
+        case chatModel
+    }
+}
--- Example/Example/ViewController/ViewController.swift
+++ Example/Example/ViewController/ViewController.swift
@@ -8,35 +8,54 @@
 import UIKit
 import JavaScriptCore
 import WebKit
+import WWPrint
+import WWToast
 import WWNetworking
 import WWEventSource
 import WWSimpleAI_Ollama
 import WWKeyboardShadowView
 import WWExpandableTextView
+import WWUserDefaults
 
 // MARK: - ViewController
 final class ViewController: UIViewController {
     
-    @IBOutlet weak var myWebView: WKWebView!
     @IBOutlet weak var connentView: UIView!
+    @IBOutlet weak var generateLiveButton: UIButton!
+    @IBOutlet weak var myWebView: WKWebView!
     @IBOutlet weak var keyboardConstraintHeight: NSLayoutConstraint!
     @IBOutlet weak var keyboardShadowView: WWKeyboardShadowView!
     @IBOutlet weak var expandableTextView: WWExpandableTextView!
     
-    private let apiUrlString = "http://localhost:11434/api/generate"
-    private let chatModel = "llama3.2"
+    @WWUserDefaults("IP") private var ip: String?
+    @WWUserDefaults("Port") private var port: String?
+    @WWUserDefaults("ChatModel") private var chatModel: String?
     
-    private var isDismiss = false
-    private var responseString: String = ""
     private var botTimestamp: Int?
-    
+    private var responseString: String = ""
+    private var lastContext: [Int]?
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         initSetting()
     }
     
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        configure(ip: ip, port: port, model: chatModel)
+    }
+    
     @IBAction func generateLiveDemo(_ sender: UIButton) {
         generateLiveAction()
+    }
+    
+    @IBAction func configureAction(_ sender: UIBarButtonItem) {
+        presentOllamaConfigureAlert()
+    }
+    
+    @IBAction func cancelBotChat(_ sender: UIBarButtonItem) {
+        WWEventSource.shared.disconnect()
+        removeLastBot(with: myWebView)
     }
 }
 
@@ -50,13 +69,13 @@ extension ViewController: WWEventSource.Delegate {
     func serverSentEventsRawData(_ eventSource: WWEventSource, result: Result<WWEventSource.RawInformation, any Error>) {
         
         switch result {
-        case .failure(let error): print(error)
+        case .failure(let error): wwPrint(error)
         case .success(let rawInformation): sseRawString(eventSource: eventSource, rawInformation: rawInformation)
         }
     }
     
     func serverSentEvents(_ eventSource: WWEventSource, eventValue: WWEventSource.EventValue) {
-        print(eventValue)
+        wwPrint(eventValue)
     }
 }
 
@@ -68,7 +87,7 @@ extension ViewController: WWKeyboardShadowView.Delegate {
     }
     
     func keyboardView(_ view: WWKeyboardShadowView, error: WWKeyboardShadowView.CustomError) {
-        print(error)
+        wwPrint(error)
     }
 }
 
@@ -79,7 +98,7 @@ private extension ViewController {
     func initSetting() {
         initKeyboardShadowViewSetting()
         initExpandableTextViewSetting()
-        initWebView()
+        initWebView(filename: "index.html")
     }
     
     /// 初始化鍵盤高度設定
@@ -91,14 +110,16 @@ private extension ViewController {
     
     /// 初始化設定可變高度的TextView (最高3行)
     func initExpandableTextViewSetting() {
+        generateLiveButton(isEnabled: false)
         expandableTextView.configure(lines: 3, gap: 21)
         expandableTextView.setting(font: .systemFont(ofSize: 20), textColor: .white, backgroundColor: .white.withAlphaComponent(0.2), borderWidth: 1, borderColor: .white)
     }
     
     /// 初始化WebView
-    func initWebView() {
+    /// - Parameter filename: HTML檔案名稱
+    func initWebView(filename: String) {
         
-        _ = myWebView._loadFile(filename: "index.html")
+        _ = myWebView._loadFile(filename: filename)
         
         myWebView.backgroundColor = .clear
         myWebView.scrollView.backgroundColor = .clear
@@ -109,20 +130,38 @@ private extension ViewController {
 // MARK: - 小工具
 private extension ViewController {
     
+    /// 參數設定
+    /// - Parameters:
+    ///   - ip: String?
+    ///   - port: String?
+    ///   - model: String?
+    func configure(ip: String?, port: String?, model: String?) {
+        
+        guard let ip, let port, let model else { return }
+        
+        WWSimpleAI.Ollama.configure(baseURL: "http://\(ip):\(port)", model: model)
+        checkConfigure()
+    }
+    
     /// 及時回應 (SSE)
     /// - Parameters:
     ///   - prompt: 提問文字
     func liveGenerate(prompt: String) {
         
+        guard let chatModel else { return }
+        
+        let urlString = WWSimpleAI.Ollama.API.generate.url()
+        
         let json = """
         {
           "model": "\(chatModel)",
           "prompt": "\(prompt)",
+          "context": \(lastContext ?? []),
           "stream": true
         }
         """
         
-        _ = WWEventSource.shared.connect(httpMethod: .POST, delegate: self, urlString: apiUrlString, httpBodyType: .string(json))
+        _ = WWEventSource.shared.connect(httpMethod: .POST, delegate: self, urlString: urlString, httpBodyType: .string(json))
     }
     
     /// 問問題 (執行SSE串流)
@@ -144,21 +183,25 @@ private extension ViewController {
     ///   - result: Result<WWEventSource.Constant.ConnectionStatus, any Error>
     func sseStatusAction(eventSource: WWEventSource, result: Result<WWEventSource.ConnectionStatus, any Error>) {
         
-        switch result {
-        case .failure(let error):
+        DispatchQueue.main.async {
             
-            DispatchQueue.main.async { [unowned self] in
-                isDismiss = true
-                responseString = ""
-                print(error)
-            }
-            
-        case .success(let status):
-            
-            switch status {
-            case .connecting: isDismiss = false; DispatchQueue.main.async { self.expandableTextView.text = ""; self.expandableTextView.updateHeight() }
-            case .open: if !isDismiss { DispatchQueue.main.async { [unowned self] in isDismiss = true }}
-            case .closed: isDismiss = false; responseString = ""
+            switch result {
+            case .failure(let error):
+                self.responseString = ""
+                self.generateLiveButton(isEnabled: true)
+                WWToast.shared.makeText("\(error.localizedDescription)")
+                
+            case .success(let status):
+                switch status {
+                case .connecting:
+                    self.expandableTextView.text = ""
+                    self.expandableTextView.updateHeight()
+                    self.generateLiveButton(isEnabled: false)
+                case .open: break
+                case .closed: 
+                    self.responseString = ""
+                    self.generateLiveButton(isEnabled: true)
+                }
             }
         }
     }
@@ -170,9 +213,7 @@ private extension ViewController {
     func sseRawString(eventSource: WWEventSource, rawInformation: WWEventSource.RawInformation) {
         
         defer {
-            DispatchQueue.main.async { [unowned self] in
-                refreashWebSlaveCell(with: myWebView, responseString: responseString)
-            }
+            DispatchQueue.main.async { [unowned self] in refreashWebSlaveCell(with: myWebView, responseString: responseString) }
         }
         
         if rawInformation.response.statusCode != 200 {
@@ -180,12 +221,15 @@ private extension ViewController {
         }
         
         guard let jsonObject = rawInformation.data._jsonObject() as? [String: Any],
-              let _response = jsonObject["response"] as? String
+              let response = jsonObject["response"] as? String,
+              let isDone = jsonObject["done"] as? Bool
         else {
             return
         }
         
-        responseString += _response
+        responseString += response
+        
+        if isDone { lastContext = jsonObject["context"] as? [Int] }
     }
 }
 
@@ -211,8 +255,8 @@ private extension ViewController {
         webView._evaluateJavaScript(script: jsCode) { result in
             
             switch result {
-            case .failure(let error): print(error)
-            case .success(let value): print(value ?? "")
+            case .failure(let error): wwPrint(error)
+            case .success(let value): wwPrint(value ?? "")
             }
         }
     }
@@ -256,5 +300,105 @@ private extension ViewController {
                 return result(dict)
             }
         }
+    }
+    
+    /// 移除最後一個Bot對話
+    /// - Parameters:
+    ///   - webView: WKWebView
+    ///   - responseString: String
+    func removeLastBot(with webView: WKWebView) {
+        
+        let jsCode = """
+            window.removeLastBot()
+        """
+        
+        webView._evaluateJavaScript(script: jsCode) { result in
+            
+            switch result {
+            case .failure(let error): wwPrint(error)
+            case .success(let value): wwPrint(value ?? "")
+            }
+            
+            self.generateLiveButton(isEnabled: true)
+        }
+    }
+    
+    /// 顯示Ollama參數設定的UIAlertController
+    /// - Parameters:
+    ///   - title: String
+    ///   - message: String?
+    func presentOllamaConfigureAlert(title: String = "本機Ollama參數設定", message: String? = nil) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in textField.text = self.ip; textField.placeholder = "127.0.0.1" }
+        alertController.addTextField { (textField) in textField.text = self.port; textField.placeholder = "11434" }
+        alertController.addTextField { (textField) in textField.text = self.chatModel; textField.placeholder = "llama3.2" }
+        
+        let sureAction = UIAlertAction(title: "確定", style: .destructive) { aciton in
+            guard let textFields = alertController.textFields else { return }
+            self.ollamaConfigure(textFields: textFields)
+        }
+        
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel) { _ in }
+        
+        alertController.addAction(sureAction)
+        alertController.addAction(cancelAction)
+
+        present(alertController, animated: true)
+    }
+    
+    /// Ollama參數設定
+    /// - Parameter textFields: [UITextField]
+    func ollamaConfigure(textFields: [UITextField]) {
+                
+        for (index, textField) in textFields.enumerated() {
+            
+            guard let type = TextFieldType(rawValue: index),
+                  let value = textField.text
+            else {
+                return
+            }
+            
+            switch type {
+            case .ip: self.ip = value
+            case .port: self.port = value
+            case .chatModel: self.chatModel = value
+            }
+        }
+        
+        self.configure(ip: self.ip, port: self.port, model: self.chatModel)
+    }
+    
+    /// 檢測參數設定是否正確
+    func checkConfigure() {
+        
+        _ = WWNetworking.shared.request(urlString: WWSimpleAI.Ollama.API.version.url(), timeout: 5) { result in
+            
+            DispatchQueue.main.async {
+                
+                switch result {
+                case .failure(let error): self.presentOllamaConfigureAlert(message: error.localizedDescription)
+                case .success(let info):
+                    
+                    guard let data = info.data,
+                          let jsonObject = data._jsonObject() as? [String: Any],
+                          let version = jsonObject["version"]
+                    else {
+                        self.generateLiveButton(isEnabled: false); return
+                    }
+                    
+                    let text = "您使用的Ollama版本為：\(version)，模型為：\(WWSimpleAI.Ollama.model)"
+                    self.generateLiveButton(isEnabled: true)
+                    WWToast.shared.makeText(text)
+                }
+            }
+        }
+    }
+    
+    /// 設定generateLiveButton是否可以使用
+    /// - Parameter isEnabled: Bool
+    func generateLiveButton(isEnabled: Bool) {
+        generateLiveButton.isEnabled = isEnabled
     }
 }
